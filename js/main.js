b6ac0ccf4e3190479d72c97f2b78c9057c6532d1
Mavlon4658 <mavlonbe4658>
2024-09-18 10:58:42 +0500

    added swiper

--- js/main.js
+++ js/main.js
@@ -9,3 +9,27 @@ header_burger.addEventListener('click', () => {
 menu_close.addEventListener('click', () => {
   menu.classList.remove('active')
 })
+
+const homeSwpBtns = document.querySelectorAll('.home_btns button');
+
+const homeSwp = new Swiper('.home .swiper', {
+  slidesPerView: 1,
+  spaceBetween: window.innerWidth / 2,
+  allowTouchMove: false,
+})
+
+homeSwp.on('slideChange', function (e) {
+  homeSwpBtns.forEach((btn, btnID) => {
+    if (homeSwp.realIndex == btnID) {
+      btn.classList.add('active');
+    } else {
+      btn.classList.remove('active');
+    }
+  })
+});
+
+homeSwpBtns.forEach((btn, btnID) => {
+  btn.onclick = () => {
+    homeSwp.slideTo(btnID);
+  }
+})
